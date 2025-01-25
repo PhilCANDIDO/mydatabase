6557d7c8f0db9ceab3154af68aea4aa88cef3a4f
Philippe CANDIDO <philippe.candido@cpf-informatique.fr>
2025-01-25 15:48:33 +0100

    Init teminate

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -52,6 +52,7 @@ new class extends Component
                     </x-slot>
 
                     <x-slot name="content">
+                        @ray('Welcome')
                         <x-dropdown-link :href="route('profile')" wire:navigate>
                             {{ __('Profile') }}
                         </x-dropdown-link>
