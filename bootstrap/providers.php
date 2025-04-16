19604e156cd60d6b1964835b9aec82faa5c6092c
Philippe CANDIDO <philippe.candido@cpf-informatique.fr>
2025-04-16 15:45:35 +0200

    add user préfence

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,6 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\PermissionServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
+    App\Providers\UserPreferenceServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
-    App\Providers\PermissionServiceProvider::class,
 ];
