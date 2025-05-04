a3be42f3c9d270a4657c5322cb2c799302280f34
Philippe CANDIDO <philippe.candido@cpf-informatique.fr>
2025-05-04 17:34:51 +0200

    Update seed

--- database/seeders/ApplicationsSeeder.php
+++ database/seeders/ApplicationsSeeder.php
@@ -15,38 +15,223 @@ class ApplicationsSeeder extends Seeder
     {
         $applications = [
             [
-                'application_name' => 'Eau de parfums',
-                'application_desc' => 'Applications pour les eaux de parfums.',
+                'application_name' => 'Fine fragrance',
+                'application_desc' => '',
                 'application_active' => true,
             ],
             [
-                'application_name' => 'Cosmétiques',
-                'application_desc' => 'Applications pour les produits cosmétiques.',
-                'application_active' => true,
-            ],
-            [
-                'application_name' => 'Shampoing',
-                'application_desc' => 'Applications pour les shampoings.',
-                'application_active' => true,
-            ],
-            [
-                'application_name' => 'Hygiène',
-                'application_desc' => 'Applications pour les produits d\'hygiène.',
-                'application_active' => true,
-            ],
-            [
-                'application_name' => 'Bougie',
-                'application_desc' => 'Applications pour les bougies parfumées.',
+                'application_name' => 'Cosmétique',
+                'application_desc' => '',
                 'application_active' => true,
             ],
             [
                 'application_name' => 'Ambiance',
-                'application_desc' => 'Applications pour les parfums d\'ambiance.',
+                'application_desc' => '',
                 'application_active' => true,
             ],
             [
                 'application_name' => 'Détergence',
-                'application_desc' => 'Applications pour les produits de détergence parfumés.',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'EDT/EDP',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Brumes',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Roll on',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Eau de cologne',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Crème solaire',
+                'application_desc' => '',
+                'application_active' => true,
+            ],	
+            [
+                'application_name' => 'Savon solide',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Gel douche',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Shampooing',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Après shampooing',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Crème/huile/sérum visage',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Crème/huile corps',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Gommage',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Déodorant',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Masque capillaire',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Mousse à raser',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Crème éclaircissante',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Maquillage',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Gel hydroalcoolique',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Crème mains',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Produits pour cheveux',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Talc',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Hairmist',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Gel intime/lubrifiant',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Bougie',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Diffuseur capillaire',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Diffuseur par nébulisation',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Diffuseur voiture',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Spray d’ambiance',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Désodorisant',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Bakhoor',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Lessive',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Assouplissant',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Lave sol',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Lave vitre',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Liquide vaisselle',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Désinfectant',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Savon liquide', 
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Antitranspirant',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Gel WC',
+                'application_desc' => '',
+                'application_active' => true,
+            ],
+            [
+                'application_name' => 'Javel',
+                'application_desc' => '',
                 'application_active' => true,
             ],
         ];
--- database/seeders/OlfactiveFamiliesSeeder.php
+++ database/seeders/OlfactiveFamiliesSeeder.php
@@ -20,23 +20,8 @@ class OlfactiveFamiliesSeeder extends Seeder
                 'olfactive_family_active' => true,
             ],
             [
-                'olfactive_family_name' => 'Florale Orientale',
-                'olfactive_family_desc' => 'Mariage de fleurs et de notes chaudes (ambre, épices douces, muscs).',
-                'olfactive_family_active' => true,
-            ],
-            [
-                'olfactive_family_name' => 'Orientale',
-                'olfactive_family_desc' => 'Notes riches et chaleureuses : ambre, vanille, résines, épices.',
-                'olfactive_family_active' => true,
-            ],
-            [
                 'olfactive_family_name' => 'Boisée',
                 'olfactive_family_desc' => 'Notes sèches ou chaudes de bois (cèdre, santal, vétiver, patchouli...).',
-                'olfactive_family_active' => true,
-            ],
-            [
-                'olfactive_family_name' => 'Boisée Orientale',
-                'olfactive_family_desc' => 'Combinaison de bois et de notes ambrées, souvent masculines.',
                 'olfactive_family_active' => true,
             ],
             [
@@ -53,40 +38,15 @@ class OlfactiveFamiliesSeeder extends Seeder
                 'olfactive_family_name' => 'Chyprée',
                 'olfactive_family_desc' => 'Accords de mousse, patchouli, bergamote, souvent sophistiqués.',
                 'olfactive_family_active' => true,
-            ],
-            [
-                'olfactive_family_name' => 'Aromatique',
-                'olfactive_family_desc' => 'Herbes (romarin, thym), lavande, très utilisés pour les parfums masculins.',
-                'olfactive_family_active' => true,
-            ],
-            [
-                'olfactive_family_name' => 'Aquatique',
-                'olfactive_family_desc' => 'Notes marines, iodées, ozoniques pour un effet de fraîcheur "eau".',
-                'olfactive_family_active' => true,
-            ],
-            [
-                'olfactive_family_name' => 'Verte',
-                'olfactive_family_desc' => 'Accord herbacé, croquant, feuilles écrasées, galbanum...',
-                'olfactive_family_active' => true,
-            ],
+            ],    
             [
                 'olfactive_family_name' => 'Fruitée',
                 'olfactive_family_desc' => 'Fruits sucrés ou acidulés, souvent exotiques (pêche, pomme, fruits rouges...).',
                 'olfactive_family_active' => true,
             ],
             [
-                'olfactive_family_name' => 'Gourmande',
-                'olfactive_family_desc' => 'Notes sucrées, alimentaires : vanille, caramel, praline, café...',
-                'olfactive_family_active' => true,
-            ],
-            [
-                'olfactive_family_name' => 'Cuirée',
-                'olfactive_family_desc' => 'Notes animales, fumées, bois brûlés ou tabac.',
-                'olfactive_family_active' => true,
-            ],
-            [
-                'olfactive_family_name' => 'Musquée',
-                'olfactive_family_desc' => 'Notes douces et sensuelles évoquant la peau propre, muscs blancs modernes.',
+                'olfactive_family_name' => 'Ambrée',
+                'olfactive_family_desc' => 'Notes chaudes et sensuelles, souvent orientales (vanille, résines, baumes).',
                 'olfactive_family_active' => true,
             ],
         ];
