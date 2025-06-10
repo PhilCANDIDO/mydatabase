c8a5c86e443e5a20f91a3e6f664a80eecb6229c0
Philippe CANDIDO <philippe.candido@cpf-informatique.fr>
2025-06-10 10:37:03 +0200

    remove telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,7 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\PermissionServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
     App\Providers\UserPreferenceServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];
